3ef986802d76c5b4f9f469d4628a2a122fdc6130
Ghada | غادة <ghadhalomar@gmail.com>
2025-10-20 19:50:24 +0300

    Update StackArray.java

--- JavaMiniProjects/DataStructures/SearchAndStack/StackArray.java
+++ JavaMiniProjects/DataStructures/SearchAndStack/StackArray.java
@@ -40,18 +40,5 @@ public class StackArray {
         return top == -1;
     }
 
-    public static void main(String[] args) {
-        StackArray stack = new StackArray(5);
-
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-
-        System.out.println("Top element: " + stack.peek());
-
-        System.out.println("Popped: " + stack.pop());
-        System.out.println("Popped: " + stack.pop());
-
-        System.out.println("Is stack empty? " + stack.isEmpty());
-    }
 }
+
